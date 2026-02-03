bc5b54521eec59e68cd02e5902023f317321bddc
QuentinPerrusson21 <quentin.perrusson@my-digital-school.org>
2026-02-03 11:02:08 +0100

    Modif

--- mob.gd
+++ mob.gd
@@ -2,8 +2,15 @@ extends CharacterBody2D
 
 
 @onready var player = get_node("/root/Game/Player")
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 250.0
 	move_and_slide()
+
+func update_animations(direction):
+	if direction == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
