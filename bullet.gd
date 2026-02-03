a7500bd6646e866e5a0383b223054123bacc5634
QuentinPerrusson21 <quentin.perrusson@my-digital-school.org>
2026-02-03 21:47:32 +0100

    barbare clean

--- bullet.gd
+++ bullet.gd
@@ -9,7 +9,7 @@ func _physics_process(delta):
 	
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * speed * delta
-	travelled_distance = speed * delta
+	travelled_distance += speed * delta
 	if travelled_distance > range:
 		queue_free()
 
--- gun.gd
+++ gun.gd
@@ -3,16 +3,25 @@ extends Area2D
 func _physics_process(delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
+		# Ici, tu pourras remettre ton code de "mob le plus proche" si tu veux
 		var targetenemy = enemies_in_range.front()
 		look_at(targetenemy.global_position)
-		
+
 func shoot():
 	const Bullet = preload("res://bullet.tscn")
 	var new_bullet = Bullet.instantiate()
+	
 	new_bullet.global_position = %Tir.global_position
 	new_bullet.global_rotation = %Tir.global_rotation
-	%Tir.add_child(new_bullet)
-
+	
+	# CORRECTION IMPORTANTE SUR LE ADD_CHILD (voir explication plus bas)
+	# %Tir.add_child(new_bullet) -> À NE PAS FAIRE
+	get_tree().root.add_child(new_bullet) 
 
 func _on_timer_timeout() -> void:
-	shoot()
+	# C'EST ICI QUE CA SE JOUE :
+	# On regarde s'il y a des ennemis dans la zone AVANT de tirer
+	var enemies_in_range = get_overlapping_bodies()
+	
+	if enemies_in_range.size() > 0:
+		shoot()
