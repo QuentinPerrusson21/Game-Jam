839e925416004d574e0da90cca46fb704a317dbe
Florkson <loris.martin1234@gmail.com>
2026-02-04 11:37:14 +0100

    ajout barre de son

--- main_menu.gd
+++ main_menu.gd
@@ -1,23 +1,30 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var main_button: VBoxContainer = $"Main button"
+@onready var options: Panel = $"Options"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 @warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
 	pass
+	
+func _ready(): 
+	main_button.visible = true
+	options.visible = false
 
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://survivorsGame.tscn")
+
+func _on_option_pressed() -> void:
+	main_button.visible = false
+	options.visible = true
+
+	
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 
-func _on_option_pressed() -> void:
-	get_tree().change_scene_to_file("res://option_menu.tscn")
-
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://survivorsGame.tscn")
+func _on_fermer_option_pressed() -> void:
+	main_button.visible = true
+	options.visible = false
