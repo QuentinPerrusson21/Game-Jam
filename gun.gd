f5a459b5f5c3b3467e24f0583bc9111a614a28ba
QuentinPerrusson21 <quentin.perrusson@my-digital-school.org>
2026-02-04 12:09:20 +0100

    new weapon

--- gun.gd
+++ gun.gd
@@ -3,19 +3,22 @@ extends Area2D
 func _physics_process(delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
-		# Ici, tu pourras remettre ton code de "mob le plus proche" si tu veux
 		var targetenemy = enemies_in_range.front()
 		look_at(targetenemy.global_position)
 
 func shoot():
+	# --- AJOUT ICI ---
+	# On lance l'animation de tir
+	# Vérifie que ton noeud s'appelle bien "AnimatedSprite2D"
+	$WeaponPivot/AnimatedSprite2D.play("shoot")
+	# -----------------
+
 	const Bullet = preload("res://bullet.tscn")
 	var new_bullet = Bullet.instantiate()
 	
 	new_bullet.global_position = %Tir.global_position
 	new_bullet.global_rotation = %Tir.global_rotation
 	
-	# CORRECTION IMPORTANTE SUR LE ADD_CHILD (voir explication plus bas)
-	# %Tir.add_child(new_bullet) -> À NE PAS FAIRE
 	get_tree().root.add_child(new_bullet) 
 
 func _on_timer_timeout() -> void:
@@ -25,7 +28,5 @@ func _on_timer_timeout() -> void:
 		shoot()
 
 func arreter_tir():
-	
 	set_physics_process(false)
-	
 	$Timer.stop()
