5e4c111b235ecede5aa89e19cbabdf64bae628ae
QuentinPerrusson21 <quentin.perrusson@my-digital-school.org>
2026-02-06 12:08:27 +0100

    push

--- map_1.gd
+++ map_1.gd
@@ -11,5 +11,13 @@ func _ready():
 		
 		add_child(joueur)
 		
+		# --- CONNEXION AU HUD ---
+		# On vérifie si le HUD existe dans la scène
+		if has_node("HUD"):
+			# On connecte le signal 'hp_changed' du joueur à la fonction du HUD
+			# Assure-toi d'avoir créé le signal dans ton script Player !
+			joueur.hp_changed.connect($HUD.actualiser_points_de_vie)
+		# -------------------------
+		
 		GameData.temps_ecoule = 0.0  
-		GameData.chrono_actif = true 
+		GameData.chrono_actif = true
